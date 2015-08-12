211070a15bd1f6d861208faa839042cbbbf53cb2
Jack Somervell <jacksomervell@gmail.com>
2015-08-12 18:51:39 +0100

    added the mathsss I wish I could add exclamation marks because I am delighted that it all works.

--- calc_it.js
+++ calc_it.js
@@ -11,12 +11,36 @@ var op = prompt('choose an operation')
 console.log('op', op);
 
 var opArray = ["-", "+", "/", "*"]
+console.log(opArray)
 
 while (opArray.indexOf(op)=== -1) {
   op = prompt('You must select one of the four operations buddy!')
 }
 
+var num2 = prompt('choose another number')
+console.log(num2)
+
+while (isNaN(num2)) {
+  num2 = prompt('You must choose an actual number!')
+}
+
+num2 = parseFloat(num2);
+
+if (op === opArray[0]) {
+  alert(num - num2);
+}
+
+if (op === opArray[1]) {
+  alert(num + num2);
+}
+
+if (op === opArray[2]) {
+  alert(num / num2);
+}
+
+if (op === opArray[3]) {
+  alert(num * num2);
+}
 
 
-alert("Why is your console not open?") ;
 
